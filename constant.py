c1b98b96566d21c6dfb5ce431139d2d56500c1b1
Augustin Bernachon <abernachon@sfrpims.fr>
2016-02-09 17:59:15 +0100

    Update constant.py : Add controls and color constants

--- constant.py
+++ constant.py
@@ -1,20 +1,31 @@
 import os
+from pygame.locals import K_UP, K_DOWN
 
-# Screen property
+# Screen constant
 SCREEN_H = 480
 SCREEN_W = 640
 SCREEN_SIZE = (SCREEN_W, SCREEN_H)
 SCREEN_ORI = (0, 0)
 
-# Assets property
+# Assets constant
 ASSETS_DIR = "assets/"
 BG_PATH = os.path.join(os.getcwd(), ASSETS_DIR + 'Start_screen.jpg')
 RACKET_P1 = os.path.join(os.getcwd(), ASSETS_DIR + 'Racket_p1.png')
 BALL = os.path.join(os.getcwd(), ASSETS_DIR + 'Ball.png')
 
-# Racket property
+# Racket constant
 STALL = -1
 UP = 0
 DOWN = 1
 RACKET_SPEED = 20  # In pixel/second
 BALL_SPEED = 4  # In pixel/second
+
+# keys constant
+CONTROLS = {
+    UP: K_UP,
+    DOWN: K_DOWN
+}
+
+# Color constant
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
