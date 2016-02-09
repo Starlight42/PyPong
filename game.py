9554790783b706aa876909c1a54542766f5068da
Augustin Bernachon <abernachon@sfrpims.fr>
2016-02-09 17:59:58 +0100

    Update game.py : Add content for all gaming functions

--- game.py
+++ game.py
@@ -1,17 +1,48 @@
 import pygame
+from pygame.locals import *
 from constant import *
 from racket import Racket
 from ball import Ball
-from random import choice
 
 class Game():
     def __init__(self, screen: pygame.Surface):
         self.screen = screen
-        self.racket_p1 = Racket(screen)
-        #self.racket_p2 = Racket(screen)
-        self.ball = Ball(screen)
+        self.GAME_RUN = True
+        self.racket_p1 = Racket(self.screen)
+        self.ball = Ball(self.screen)
 
-    def start(self):
+    def setup(self):
+        pygame.key.set_repeat(200, 50)
+        self.GAME_RUN = True
+        self.racket_p1 = Racket(self.screen)
+        self.ball = Ball(self.screen)
+
+    def update_screen(self):
+        pygame.draw.rect(self.screen, BLACK, (0, 0) + SCREEN_SIZE)
+        pygame.draw.rect(self.screen, WHITE, ((self.screen.get_width() // 2) - 1, 0, 2,
+                                              self.screen.get_height()))
         self.racket_p1.render()
         self.ball.render()
-        self.racket_p1.move(DOWN)
+
+    def process_event(self, event: pygame.event):
+        if event.type == KEYDOWN:
+            if event.key == CONTROLS[UP]:
+                self.racket_p1.move(UP)
+            elif event.key == CONTROLS[DOWN]:
+                self.racket_p1.move(DOWN)
+
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            self.GAME_RUN = False
+            print("Ending the game...")
+
+    def start(self):
+        print("Starting the game...")
+        self.setup()
+
+        while self.GAME_RUN:
+            for event in pygame.event.get():
+                self.process_event(event)
+
+            self.update_screen()
+            self.ball.move(self.racket_p1)
+            pygame.display.flip()
