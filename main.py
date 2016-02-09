353e7f00814f64a4eecec2ef6e75305d533a4716
Augustin Bernachon <abernachon@sfrpims.fr>
2016-02-09 18:02:15 +0100

    Update main.py : Removed all debuging helper

--- main.py
+++ main.py
@@ -1,32 +1,28 @@
 import pygame
 from pygame.locals import *
 from game import Game
-from constant import *
+from constant import (SCREEN_SIZE, SCREEN_ORI,
+                      BG_PATH, BLACK)
 
 def main():
-    print("Press any key to start the game")
+    print("Press return to start the game")
 
     pygame.init()
     RUN = True
-    GAME_STATE = False
     screen = pygame.display.set_mode(SCREEN_SIZE)
     background = pygame.image.load(BG_PATH).convert_alpha()
     game = Game(screen)
 
     while RUN:
-
-        if not GAME_STATE:
-            screen.blit(background, SCREEN_ORI)
         for event in pygame.event.get():
             if event.type == QUIT or \
                     (event.type == KEYDOWN and event.key == K_ESCAPE):
                 RUN = False
             if event.type == KEYDOWN and event.key == K_RETURN:
-                #if not GAME_STATE:
-                GAME_STATE = True
                 game.start()
 
-        #screen.blit(background, SCREEN_ORI)
+        pygame.draw.rect(screen, BLACK, (0, 0) + SCREEN_SIZE)
+        screen.blit(background, SCREEN_ORI)
         pygame.display.flip()
 
     pygame.quit()
