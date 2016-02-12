d55df431422d2e8e9c922e1aeab15ff561924bc9
Augustin Bernachon <abernachon@gmail.com>
2016-02-12 17:33:10 +0100

    Major Update for game menu : Add loop and menuItem creation

--- menu.py
+++ menu.py
@@ -2,25 +2,53 @@ import pygame
 from pygame.locals import *
 
 from base_class.base_loop import BaseLoop
+from menu_item import MenuItem
+from game import Game
 from constant import *
 
 
 class MainMenu(BaseLoop):
     def __init__(self, screen: pygame.Surface):
         super(MainMenu, self).__init__(screen)
-        self.font = pygame.font.SysFont('FreeSans', 12)
+        self.game = Game(self.screen)
+        screen_portion = (self.screen.get_width() / 2, self.screen.get_height() / 3)
+        screen_portion1 = (screen_portion[0], screen_portion[1] * 1.5)
+        screen_portion2 = (screen_portion[0], screen_portion[1] * 2)
+
+        self.menu = [MenuItem(self.screen, '1 PLAYER', screen_portion),
+                     MenuItem(self.screen, '2 PLAYER', screen_portion1),
+                     MenuItem(self.screen, 'QUIT MENU', screen_portion2)]
 
     def setup(self):
         super(MainMenu, self).setup()
+        self.menu[0].select(True)
 
     def update_screen(self):
         super(MainMenu, self).update_screen()
-        text = self.font.render('Hello Master!', True, WHITE)
-        self.screen.blit(text, (320, 240))
+        for item in self.menu:
+            if item.selected:
+                item.render(RED)
+            else:
+                item.render(BLUE)
+
+    def find_select(self, direction):
+        for index, menu in enumerate(self.menu):
+            if menu.selected:
+                menu.select(False)
+                if direction is UP:
+                    self.menu[index - 1].select(True)
+                elif direction is DOWN:
+                    self.menu[0 if (index+1) == len(self.menu) else index + 1].select(True)
+                break
 
     def process_event(self, event: pygame.event):
         if event.type == KEYDOWN:
-            pass
+            if event.key == CONTROLS[UP]:
+                self.find_select(UP)
+            elif event.key == CONTROLS[DOWN]:
+                self.find_select(DOWN)
+            elif event.key == K_RETURN:
+                self.game.start()
 
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             self.LOOP_RUN = False
--- menu_item.py
+++ menu_item.py
@@ -1,11 +1,27 @@
-class MenuItem():
-    def __init__(self, text=None):
-        self.text = text
-        self.rect = None
-        self.pos_rect = []
-        self.pos_text = []
-        self.rect_dim = []
-        self.text_dim = []
+import pygame
+from constant import *
 
-    def render(self):
-        pass
+class MenuItem():
+    def __init__(self, screen: pygame.Surface, text, item_center, item_size: tuple=None):
+        self.screen = screen
+        self.rect_dim = (300, 50) if item_size is None else item_size
+        self.text = text
+        self.selected = False
+        self.text_render = None
+        self.font = pygame.font.SysFont('FreeSansBold', 42)
+        self.rect = pygame.Rect(SCREEN_ORI, self.rect_dim)
+        self.text_dim = self.font.size(self.text)
+        self.rect_center = item_center
+        self.pos_text = (self.rect_center[0] - (self.text_dim[0] / 2),
+                         self.rect_center[1] - (self.text_dim[1] / 2))
+
+
+    def render(self, text_color: tuple=BLACK):
+        self.rect.center = self.rect_center
+        self.screen.fill(WHITE, self.rect)
+        self.text_render = self.font.render(self.text, True, text_color)
+        self.screen.blit(self.text_render, self.pos_text)
+
+    def select(self, is_selected: bool):
+        self.selected = is_selected
+        self.font = pygame.font.SysFont('FreeSansBold', 42, italic=is_selected)
