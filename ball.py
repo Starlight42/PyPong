2baff56d43ed67c1a7e2d0ff8ab8dd6df39f428e
Augustin Bernachon <abernachon@gmail.com>
2016-02-15 17:46:04 +0100

    Update ball class : Add edge collision method

--- ball.py
+++ ball.py
@@ -14,16 +14,21 @@ class Ball(BaseObject):
         self.pos = [(self.screen.get_width() - self.ball_w) // 2,
                     (self.screen.get_height() - self.ball_h) // 2]
 
-    def move(self, racket: Racket):
+    def check_edge_collision(self):
         if self.pos[1] <= 0 or self.pos[1] >= SCREEN_H - self.ball_h:
             self.vec[1] *= -1
         if self.pos[0] >= SCREEN_W - self.ball_w or self.pos[0] <= 0:
             self.vec[0] *= -1
 
-        if self.pos[1] == racket.pos[1] and self.pos[0] == racket.pos[0]:
-            self.vec[1] *= -1
+    def check_racket_collision(self, racket: Racket):
+        pass
+        #if self.pos[1] == racket.pos[1] and self.pos[0] == racket.pos[0]:
+        #   self.vec[1] *= -1
         #if self.pos[0] == racket.pos[0]:
         #   self.vec[0] *= -1
+
+    def move(self, racket: Racket):
+        self.check_edge_collision()
 
         self.pos[0] += self.vec[0]
         self.pos[1] += self.vec[1]
